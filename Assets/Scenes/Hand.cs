f0ad34217de31cabc968d87f41c94b5fa8e3bcf3
Cole Robinette <robinette.31@gmail.com>
2019-07-30 13:25:29 -0400

    first commit

---
+++ Assets/Scenes/Hand.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Hand : MonoBehaviour {
+    public enum HandType { left, right, editor }
+    public HandType hand;
+    Pickupable ball;
+    OVRInput.Controller controller;
+    [HideInInspector] public Pickupable contents;
+
+    void Start() {
+        controller = (hand == HandType.left) ? OVRInput.Controller.LTouch : OVRInput.Controller.RTouch;
+        ball = GameObject.FindGameObjectWithTag("ball").GetComponent<Pickupable>();
+    }
+
+    void Update() {
+        float trigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+        if (hand == HandType.editor) {
+            trigger = (Input.GetKey(KeyCode.Space)) ? 1.0f : 0.0f;
+        }
+        if (trigger > 0.5f) {
+            if (contents == null) {
+                contents = ball;
+                contents.PickUp(this);
+            }
+        } else if (contents != null) {
+            if (hand == HandType.editor) {
+                contents.Throw(new Vector3(0, 0.5f, 1) * 9, Vector3.zero);
+            } else {
+                contents.Throw(OVRInput.GetLocalControllerVelocity(controller), OVRInput.GetLocalControllerAngularVelocity(controller));
+            }
+            contents = null;
+        }
+    }
+}
---
+++ Assets/Scenes/Pickupable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Pickupable : MonoBehaviour {
+    Hand hand;
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (hand != null) {
+            transform.position = hand.transform.position;
+            transform.rotation = hand.transform.rotation;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+    }
+
+    public void PickUp(Hand target) {
+        Debug.Log(gameObject.name + " picked up by " + target.gameObject.name);
+        if (hand != null) {
+            hand.contents = null;
+        }
+        hand = target;
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    public void Throw(Vector3 velocity, Vector3 angularVelocity) {
+        Debug.Log(gameObject.name + " thrown by " + hand.gameObject.name + " with velocity " + velocity);
+        hand = null;
+        rb.isKinematic = false;
+        rb.velocity = velocity;
+        rb.angularVelocity = angularVelocity;
+    }
+}
